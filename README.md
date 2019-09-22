1640fe6d38ad28d88587312c3615b555ec6d341b
Sotiraki Sima <sotiraki.sima@onefirewall.com>
2019-09-22 21:30:37 +0100

    v 0.2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # secret-shark
 Search for confidential information
+
+## How to run? (Linux only)
+
+bash init.sh /folder/you/want/to/scan
---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo -e \
+"
+##########################################################
+#                                                        #
+#          Search for confidential information           #
+#                    Version: 0.2                        #
+#                Author: Sotiraki Sima                   #
+#                                                        #
+##########################################################
+"
+my_array=(
+    "(=\ |=)[A-Z0-9]{20}$" 
+    "(=\ |=)[A-Z0-9]{40}$"
+    "(access_key_id|secret_access_key)"
+    "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"
+)
+
+for i in "${my_array[@]}"; 
+do 
+    grep -nrIi -E "$i" --exclude-dir=.git --exclude-dir=node_modules --color=always $1 | cut -c -200
+done
+
