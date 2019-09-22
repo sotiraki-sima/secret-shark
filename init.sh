a01607a1d6253009a398fbb59411140312a474af
Sotiraki Sima <sotiraki.sima@onefirewall.com>
2019-09-22 22:00:21 +0100

    add file search capability

--- init.sh
+++ init.sh
@@ -10,14 +10,46 @@ echo -e \
 #                                                        #
 ##########################################################
 "
-my_array=(
+regex_array=(
     "(=\ |=)[A-Z0-9]{20}$" 
     "(=\ |=)[A-Z0-9]{40}$"
     "(access_key_id|secret_access_key)"
+)
+
+no_in_use_regex_array=(
     "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"
 )
 
-for i in "${my_array[@]}"; 
+
+files_array=(
+    "*.pem"
+    "id_rsa"
+    "authorized_keys"
+)
+
+echo -e \
+"
+##########################################################
+#                     Find Command                       #
+##########################################################
+"
+
+for i in "${files_array[@]}"; 
 do 
-    grep -nrIi -E "$i" --exclude-dir=.git --exclude-dir=.svn --exclude-dir=node_modules --color=always $1 | cut -c -200
+    find $1 -name "$i" 2>/dev/null
+done
+
+
+
+
+echo -e \
+"
+##########################################################
+#                     Grep Command                       #
+##########################################################
+"
+
+for i in "${regex_array[@]}"; 
+do 
+    grep -nrI -E "$i" --exclude-dir=.git --exclude-dir=.svn --exclude-dir=node_modules --color=always $1 | cut -c -200 2>/dev/null
 done
